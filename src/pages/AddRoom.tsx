f4713fc4b4d28456896ce4a1d393738e637f315f
Lais Frigério <laiss@gazetadopovo.com.br>
2021-06-23 20:53:55 -0300

    Add Home and AddRoom pages

---
+++ src/pages/AddRoom.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import PageAuth from '../components/PageAuth'
+import TheAside from '../ui/TheAside'
+import TheButton from '../ui/TheButton'
+
+import logoImg from '../assets/images/logo.svg'
+
+export default function AddRoom () {
+  return (
+    <PageAuth>
+      <TheAside />
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Ask me Bro" />
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+            />
+            <TheButton type="submit">Criar sala</TheButton>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui!</Link>
+          </p>
+        </div>
+      </main>
+    </PageAuth>
+  )
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+import PageAuth from '../components/PageAuth'
+import TheAside from '../ui/TheAside'
+import TheButton from '../ui/TheButton'
+
+import logoImg from '../assets/images/logo.svg'
+import goggleIconImg from '../assets/images/google-icon.svg'
+
+export default function Home () {
+  const history = useHistory()
+
+  const navigateTo = () => {
+    history.push('/rooms/add')
+  }
+
+  return (
+    <PageAuth>
+      <TheAside />
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Ask me Bro" />
+          <TheButton onClick={navigateTo} className="btn-google create-room">
+            <img src={goggleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </TheButton>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <TheButton type="submit">Entrar na sala</TheButton>
+          </form>
+        </div>
+      </main>
+    </PageAuth>
+  )
+}
