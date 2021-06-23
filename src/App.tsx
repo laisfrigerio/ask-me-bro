4bfef9cdfec60b0a59ba40d705c70e6fd4cdbda8
Lais Frigério <laiss@gazetadopovo.com.br>
2021-06-23 20:54:54 -0300

    Add global styles, TheButton and TheAside component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import './services/firebase'
 
-import Wrapper from './components/Wrapper'
+import AddRoom from './pages/AddRoom'
+import Home from './pages/Home'
 import GlobalStyle from './global/style'
-import askLogo from './images/ask.png'
 
 function App() {
   return (
     <React.Fragment>
       <GlobalStyle />
-      <Wrapper>
-        <div className="logo">
-          <img src={askLogo} alt="Ask me Bro" title="Ask me Bro" />-me-bro
-        </div>
-        <p className="comming-soon">comming soon ...</p>
-      </Wrapper>
+      <BrowserRouter>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/add" component={AddRoom} />
+      </BrowserRouter>
     </React.Fragment>
   )
 }
