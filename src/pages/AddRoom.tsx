db42029895138a67597e02614977b78f60da57b3
Lais Frigério <laiss@gazetadopovo.com.br>
2021-06-25 23:48:33 -0300

    Add new room

--- src/pages/AddRoom.tsx
+++ src/pages/AddRoom.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { FormEvent, useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 
 import PageAuth from '../components/PageAuth'
 import TheAside from '../ui/TheAside'
 import TheButton from '../ui/TheButton'
 
 import logoImg from '../assets/images/logo.svg'
+import { database } from '../services/firebase'
+import { useAuth } from '../hooks/useAuth'
 
 export default function AddRoom () {
+  const { user } = useAuth()
+  const history = useHistory()
+  const [roomName, setRoomName] = useState('')
+
+  async function handleAddRoom (event: FormEvent) {
+    event.preventDefault()
+    
+
+    if (roomName.trim() === '') {
+      return
+    }
+
+    const ref = database.ref('rooms')
+    const firebase = await ref.push({ name: roomName, authorId: user?.id })
+
+    history.push(`/rooms/${firebase.key}`)
+  }
 
   return (
     <PageAuth>
@@ -16,10 +35,12 @@ export default function AddRoom () {
         <div className="main-content">
           <img src={logoImg} alt="Ask me Bro" />
           <h2>Criar uma nova sala</h2>
-          <form>
+          <form onSubmit={handleAddRoom}>
             <input
               type="text"
               placeholder="Nome da sala"
+              value={roomName}
+              onChange={(event) => setRoomName(event.target.value)}
             />
             <TheButton type="submit">Criar sala</TheButton>
           </form>
