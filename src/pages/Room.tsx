b42204290c32af80acb800b49a8cd9ce0e4682b7
Lais Frigério <laiss@gazetadopovo.com.br>
2021-06-26 11:26:10 -0300

    Load questions from room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from "react"
+import React, { FormEvent, useState, useEffect } from "react"
 import TheButton from '../ui/TheButton'
 import TheHeader from '../ui/TheHeader'
 import TheMainContent from '../ui/TheMainContent'
@@ -6,69 +6,124 @@ import { useAuth } from "../hooks/useAuth"
 import { database } from "../services/firebase"
 import { useParams } from "react-router-dom"
 
+type QuestionsType = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  },
+  content: string;
+  isHighlighted: boolean;
+  isAnswered: boolean;
+}
+
+type FirebaseQuestionsProps = Record<
+  string,
+  {
+    author: {
+      name: string;
+      avatar: string;
+    };
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
+    likes: Record<
+      string,
+      {
+        authorId: string;
+      }
+    >;
+  }
+>;
+
 type ParamsType = {
-	id: string
+  id: string
 }
 
 export default function Room () {
-	const params = useParams<ParamsType>()
-	const roomId = params.id
+  const params = useParams<ParamsType>()
+  const roomId = params.id
 
-	const [question, setQuestion] = useState('')
-	const { user } = useAuth()
+  const [question, setQuestion] = useState('')
+  const [roomName, setRoomName] = useState('')
+  const [questions, setQuestions] = useState<QuestionsType[]>([])
+  const { user } = useAuth()
 
-	async function handleAddQuestion (event: FormEvent) {
-		event.preventDefault()
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`)
 
-		if (question.trim() === '') {
-			return
-		}
+    roomRef.on('value', room => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestionsProps = databaseRoom.questions ?? {};
 
-		if (!user) {
-			alert('You must logged in before send questions')
-		}
+      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+        return {
+          id: key,
+          content: value.content,
+          author: value.author,
+          isHighlighted: value.isHighlighted,
+          isAnswered: value.isAnswered,
+        }
+      })
 
-		const payload = {
-			content: question,
-			author: {
-				name: user?.name,
-				avatar: user?.avatar
-			},
-			isHighlighted: false,
-			isAnswered: false
-		}
+      setRoomName(databaseRoom.name);
+      setQuestions(parsedQuestions);
+    })
+  }, [roomId]);
 
-		await database.ref(`rooms/${roomId}/questions`).push(payload)
-		setQuestion('')
-	}
+  async function handleAddQuestion (event: FormEvent) {
+    event.preventDefault()
 
-	return (
-		<React.Fragment>
-			<TheHeader />
-			<TheMainContent>
-				<div className="room-title">
-					<h1 className="title-01">Sala React Q&A</h1>
-					<span>4 perguntas</span>
-				</div>
-				<form onSubmit={handleAddQuestion}>
-					<textarea
-						placeholder="O que você quer perguntar?"
-						value={question}
-						onChange={event => setQuestion(event.target.value)}
-					/>
-					<div className="form-footer">
-						{user ? (
-							<div className="user-info">
-								<img src={user.avatar} alt={user.name} />
-								<span>{user.name}</span>
-							</div>
-						) : (
-							<p>Para enviar uma pergunta, <a className="btn-as-link" href="#login">Faça seu login</a></p>
-						)}
-						<TheButton>Enviar pergunta</TheButton>
-					</div>
-				</form>
-			</TheMainContent>
-		</React.Fragment>
-	)
+    if (question.trim() === '') {
+      return
+    }
+
+    if (!user) {
+      alert('You must logged in before send questions')
+    }
+
+    const payload = {
+      content: question,
+      author: {
+        name: user?.name,
+        avatar: user?.avatar
+      },
+      isHighlighted: false,
+      isAnswered: false
+    }
+
+    await database.ref(`rooms/${roomId}/questions`).push(payload)
+    setQuestion('')
+  }
+
+  return (
+    <React.Fragment>
+      <TheHeader />
+      <TheMainContent>
+        <div className="room-title">
+          <h1 className="title-01">Sala {roomName}</h1>
+          { questions.length > 0 && <span>{questions.length} pergunta(s)</span> }
+        </div>
+        <form onSubmit={handleAddQuestion}>
+          <textarea
+            placeholder="O que você quer perguntar?"
+            value={question}
+            onChange={event => setQuestion(event.target.value)}
+          />
+          <div className="form-footer">
+            {user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <p>Para enviar uma pergunta, <a className="btn-as-link" href="#login">Faça seu login</a></p>
+            )}
+            <TheButton>Enviar pergunta</TheButton>
+          </div>
+        </form>
+        {JSON.stringify(questions)}
+      </TheMainContent>
+    </React.Fragment>
+  )
 }
